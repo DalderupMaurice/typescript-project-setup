682d04903cb1e936eb1309eb66c9bd1822701440
Maurice Dalderup <10922045+DalderupMaurice@users.noreply.github.com>
2021-09-15 19:20:44 +0200

    fix eslint with pnp

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 (() => {
-	console.log(Math.PI);
-	console.log(Math.PI);
-	console.log(Math.PI);
-	console.log(Math.PI);
+  console.log(Math.PI);
+  console.log(Math.PI);
+  console.log(Math.PI);
+  console.log(Math.PI);
 })();
